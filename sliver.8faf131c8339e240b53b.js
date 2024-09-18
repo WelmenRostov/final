dfb0823d29d0fe1acebf3d5ee7e8b56a5833cd29
Nemakhov Andrei <welmenrostov@gmail.com>
2024-09-19 00:28:13 +0300

    initial commit

---
+++ sliver.8faf131c8339e240b53b.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/slider_swiper.js":
+/*!*****************************!*\
+  !*** ./js/slider_swiper.js ***!
+  \*****************************/
+/***/ (() => {
+
+eval("var swiperСompany = new Swiper(\".swiperСompany\", {\r\n    direction: \"horizontal\",\r\n    centeredSlides: false,\r\n    slidesPerView: \"auto\",\r\n    pagination: {\r\n        el: \".swiper-paginationCompany\",\r\n        clickable: true,\r\n    },\r\n\r\n    breakpoints: {\r\n        768: {\r\n            enabled: false,\r\n        },\r\n    },\r\n    loop: true,\r\n    keyboard: {\r\n        enabled: true,\r\n        onlyInViewport: true,\r\n        pageUpDown: true,\r\n    },\r\n    mousewheel: {\r\n        sensitivity: 1,\r\n    }\r\n});\r\n\r\nvar swiperTechnic = new Swiper(\".swiperTechnic\", {\r\n    direction: \"horizontal\",\r\n    centeredSlides: false,\r\n    slidesPerView: \"auto\",\r\n    spaceBetween: 14,\r\n    pagination: {\r\n        el: \".swiper-paginationTechnic\",\r\n        clickable: true,\r\n    },\r\n\r\n    breakpoints: {\r\n        768: {\r\n            enabled: false,\r\n        },\r\n    },\r\n    loop: true,\r\n    keyboard: {\r\n        enabled: true,\r\n        onlyInViewport: true,\r\n        pageUpDown: true,\r\n    },\r\n    mousewheel: {\r\n        sensitivity: 2,\r\n    },\r\n});\r\n\r\nvar swiperPrise = new Swiper(\".swiperPrise\", {\r\n    direction: \"horizontal\",\r\n    centeredSlides: false,\r\n    slidesPerView: \"auto\",\r\n    spaceBetween: 14,\r\n    pagination: {\r\n        el: \".swiper-paginationPrise\",\r\n        clickable: true,\r\n    },\r\n\r\n    breakpoints: {\r\n        768: {\r\n            enabled: false,\r\n        },\r\n    },\r\n    loop: true,\r\n    keyboard: {\r\n        enabled: true,\r\n        onlyInViewport: true,\r\n        pageUpDown: true,\r\n    },\r\n    mousewheel: {\r\n        sensitivity: 2,\r\n    },\r\n});\r\n\n\n//# sourceURL=webpack:///./js/slider_swiper.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./js/slider_swiper.js"]();
+/******/ 	
+/******/ })()
+;
